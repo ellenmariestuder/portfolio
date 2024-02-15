859a2b55cd6ea2333a86d6f98fbb70c9a4560d70
Ellen <ellenmstuder@gmail.com>
2024-02-15 17:07:45 -0600

    attempted: dynamic import via lume urls

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,44 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
 module.exports = {
-  entry: './src/js/app.js', // Change to your main JavaScript file
+  entry: './src/js/app.js', // Adjust the entry point to your main JavaScript file
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
       {
-        test: /\.html$/,
-        use: ['html-loader'],
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
       },
     ],
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html', // Path to your HTML file
-    }),
-  ],
 };
+
+// const path = require('path');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// module.exports = {
+//   entry: './src/js/app.js', // Change to your main JavaScript file
+//   output: {
+//     filename: 'bundle.js',
+//     path: path.resolve(__dirname, 'dist'),
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.html$/,
+//         use: ['html-loader'],
+//       },
+//     ],
+//   },
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       template: './src/index.html', // Path to your HTML file
+//     }),
+//   ],
+// };
